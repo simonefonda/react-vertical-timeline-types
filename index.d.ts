f2c0286f9725a63190d41a5a63eba64afffb3a85
Simone Fonda <fonda@spaziodati.eu>
2018-03-14 12:47:35 +0100

    Definitions and package.json

---
+++ index.d.ts
@@ -0,0 +1,23 @@
+// Type definitions for react-vertical-timeline 2.0.5
+// Project: https://github.com/stephane-monnot/react-vertical-timeline
+// Definitions by: Simone Fonda <fonda@spaziodati.eu>
+// TypeScript Version: 2.6
+
+import * as React from 'react';
+
+
+export interface VerticalTimelineProps extends React.Props<VerticalTimeline> {
+    animate?: boolean;
+    className?: string;
+}
+
+export interface VerticalTimelineElementProps extends React.Props<VerticalTimelineElement> {
+    className?: string;
+    date?: string;
+    iconStyle?: React.CSSProperties;
+    icon?: React.ReactNode;
+    position?: string;
+}
+
+export class VerticalTimeline extends React.Component<VerticalTimelineProps> { }
+export class VerticalTimelineElement extends React.Component<VerticalTimelineElementProps> { }
